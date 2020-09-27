c81308391dd8ed06d925a08922f8f5084dbfeef2
D-jeme <eavdispahi1@hotmail.com>
2020-09-27 14:55:00 +0200

    itze

--- frontend/src/app/models/artikal.ts
+++ frontend/src/app/models/artikal.ts
@@ -5,8 +5,9 @@ export class Artikal {
     dugi_tekst: String;
     cijena: number;
     popust: number;
-    kolicina
-  constructor(id: number, naziv: String, kratki_tekst: String, dugi_tekst: String, cijena: number, popust: number, kolicina: number) {
+    kolicina:number;
+    img:String;
+  constructor(id: number, naziv: String, kratki_tekst: String, dugi_tekst: String, cijena: number, popust: number, kolicina: number,img:String) {
     this.id = id;
     this.naziv=naziv;
     this.kratki_tekst=kratki_tekst;
@@ -14,6 +15,7 @@ export class Artikal {
     this.cijena=cijena;
     this.popust=popust;
     this.kolicina=kolicina;
+    this.img=img;
   }
 
 }
